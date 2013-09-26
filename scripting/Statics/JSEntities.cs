02a6d1b6b5d4563a083a8f02f9d2c09a77792856
oobenoob_cp <SND\oobenoob_cp>
2013-09-26 14:37:39 +0000

    Added new static class to scripting, Entities.

---
+++ scripting/Statics/JSEntities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Jurassic;
+using Jurassic.Library;
+
+namespace scripting.Statics
+{
+    [JSEmbed(Name = "Entities")]
+    class JSEntities : ObjectInstance
+    {
+        public JSEntities(ScriptEngine engine)
+            : base(engine)
+        {
+            this.PopulateFunctions();
+        }
+
+        protected override string InternalClassName
+        {
+            get { return "Entities"; }
+        }
+
+        [JSFunction(Name = "encode", IsWritable = false, IsEnumerable = true)]
+        public static String Encode(object a)
+        {
+            if (a is Null || a is Undefined)
+                return null;
+
+            return WebUtility.HtmlEncode(a.ToString());
+        }
+
+        [JSFunction(Name = "decode", IsWritable = false, IsEnumerable = true)]
+        public static String Decode(object a)
+        {
+            if (a is Null || a is Undefined)
+                return null;
+
+            return WebUtility.HtmlDecode(a.ToString());
+        }
+    }
+}
