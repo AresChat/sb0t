62aae3dd6d5741dbdacc117c1663fa2eeacbb814
thispixel <stuart@programmer.net>
2016-07-13 23:12:12 +0100

    increment server version to 5.30

--- core/Settings.cs
+++ core/Settings.cs
@@ -13,9 +13,9 @@ namespace core
     {
         public const String RELEASE_URL = "https://github.com/AresChat/sb0t/releases";
         public const String VERSION_CHECK_URL = "https://api.github.com/repos/AresChat/sb0t/releases";
-        public const String VERSION_NUMBER = "5.29";
+        public const String VERSION_NUMBER = "5.30";
 
-        public const String VERSION = "sb0t 5.29";
+        public const String VERSION = "sb0t 5.30";
         public const ushort LINK_PROTO = 500;
 
         public static bool RUNNING { get; set; }
