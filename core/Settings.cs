9302e2d863445204b3feb3b85632dfd5576b7100
Stuart <thispixel@users.noreply.github.com>
2016-10-22 15:26:07 +0100

    update version

--- core/Settings.cs
+++ core/Settings.cs
@@ -14,7 +14,7 @@ namespace core
         public const String RELEASE_URL = "https://github.com/AresChat/sb0t/releases";
         public const String VERSION_CHECK_URL = "https://api.github.com/repos/AresChat/sb0t/releases";
         public const String BLACKLIST_URL = "https://raw.githubusercontent.com/AresChat/sb0t/master/blacklist.txt";
-        public const String VERSION_NUMBER = "5.33";
+        public const String VERSION_NUMBER = "5.34";
 
         public const String VERSION = "sb0t 5.34";
         public const ushort LINK_PROTO = 500;
